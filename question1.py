de3728e188c6cd56510d6d3ee73df977d8646b84
Julian Quintero <julianqula@unisabana.edu.co>
2026-04-24 15:53:45 -0500

    lab8-2

--- question1.py
+++ question1.py
@@ -1,14 +1,14 @@
 import sys
 try:
     if len(sys.argv) != 3:
-        sys.exit(1)
+        raise ValueError
     Argumento_uno = sys.argv[1]
     Argumento_dos = sys.argv[2]
     load_per_support = float(Argumento_uno) / float(Argumento_dos)
     if Argumento_dos == "0":
         print("Error: Cannot divide by zero! Supports must be greater than zero.")
     else:
-        print(f"Load per support: {load_per_support}")
+        print(f"Load per support: {load_per_support:.2f}N")
 except ValueError:
     print("Error: Invalid input! Enter numeric values only.")
 
