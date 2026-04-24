fddfe96cdf83afcd63272cd6d985502138f6c892
Julian Quintero <julianqula@unisabana.edu.co>
2026-04-24 15:52:09 -0500

    lab8-1

--- question1.py
+++ question1.py
@@ -1,6 +1,14 @@
-"""Laboratorio 8 - Problema 1.
+import sys
+try:
+    if len(sys.argv) != 3:
+        sys.exit(1)
+    Argumento_uno = sys.argv[1]
+    Argumento_dos = sys.argv[2]
+    load_per_support = float(Argumento_uno) / float(Argumento_dos)
+    if Argumento_dos == "0":
+        print("Error: Cannot divide by zero! Supports must be greater than zero.")
+    else:
+        print(f"Load per support: {load_per_support}")
+except ValueError:
+    print("Error: Invalid input! Enter numeric values only.")
 
-Implementa una CLI que calcule carga por punto de soporte.
-"""
-
-# TODO: Implementar según README.md
